48574aca30c0d1eda27e9b05888e61bf956e59ae
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-08-08 15:46:18 +0200

    Initial commit

---
+++ src/services/Pixabay.API.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '37644532-ea94dde14fb8169986b0fbd6c';
+
+export const perPage = 12;
+
+export const getImages = async (query, page) => {
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response.data;
+};
+export const normalizedImages = imagesArray =>
+  imagesArray.map(({ id, tags, webformatURL, largeImageURL }) => {
+    return { id, tags, webformatURL, largeImageURL };
+  });
